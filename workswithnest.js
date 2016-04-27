b53e628f742f14431a8d8063fdb41ee0ede1bb19
Iker Larizgoitia <larizgoitia@gmail.com>
2016-04-27 14:35:14 +0000

    Added use of keyboard as button

--- workswithnest.js
+++ workswithnest.js
@@ -1,10 +1,15 @@
 /***
  * CONFIGURATION VARIABLES
- */
 var userAPIKey = '';        //Your Application User API Key
 var cameraThngId = '';      //Your Camera ID in EVRYTHNG
 var activityZoneName = '';  //The activity zone name to monitor
 var raspberryPiThngId = ''; //The raspberry pi ID in EVRYTHNG
+ */
+
+var userAPIKey = 'dDRZwfzETz7G3BJ65jIXryehBYtafrYkggpzJxJ96ntoSZKzSdmbycFmetZ1mUZKrIt2GFidqC5iRLaA';
+var cameraThngId = 'UhxQBEM5sGHYQFVM4t7yxrpn';
+var activityZoneName = 'Bookshelf';
+var raspberryPiThngId = 'UYa6PhscVychVAhhbpqqhpsn';
 
 /**
  * EVRYTHNG CONFIGURATION
@@ -41,11 +46,10 @@ var activityZone = {
 var raspberryPi = null;
 user.thng(raspberryPiThngId).read().then(function (thng) {
     raspberryPi = thng;
-}
+});
 
 /**
- * RASPBERRY PI DEVICE CONFIGURATION
- */
+ * RASPBERRY PI LED and BUTTON CONFIGURATION 
 var Gpio = require('onoff').Gpio;
 var led = new Gpio(14, 'out');
 var button = new Gpio(4, 'in', 'both');
@@ -56,44 +60,79 @@ button.watch(function (err, value) {
     if (err) {
         throw err;
     }
-    clearInterval(blinkInterval);       // Stop blinking
-    led.writeSync(0);                   // Turn LED off.
-    raspberryPi.property().update({     // Update device status in the cloud
-        alarm: 'off'
-    });
+    stopAlarm();
 });
+*/
+
+var stopAlarm = function(){
+   //clearInterval(blinkInterval);       // Stop blinking
+   //led.writeSync(0);                   // Turn LED off.
+   raspberryPi.property().update({     // Update device status in the cloud
+       	alarm: 'off'
+   });
+}
+
+var startAlarm = function(){
+  console.log('Updating alarm status in the cloud');
+  raspberryPi.property().update({     // Update device status in the cloud
+          alarm: 'on'
+  });
+
+  //start blinking the LED
+  //clearInterval(blinkInterval);
+  //blinkInterval = setInterval(function () {
+  //       led.writeSync(led.readSync() === 0 ? 1 : 0)
+  //}, 500);
+
+}
+
+
+/**
+ * RASPBERRY PI CONFIGURE KEYBOARD as the button
+ */
+var keypress = require('keypress');
+
+keypress(process.stdin);
+
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.name == 'space') {
+        stopAlarm();
+  }
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+    process.exit();
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 /**
  * Nest CAMERA CONFIGURATION
  */
 //Get the camera information
 user.thng(cameraThngId).read().then(function (camera) {
+    console.log('Found camera!');
 
     //Obtain the activity zone id for the zone we want to monitor
     camera.properties.activity_zones.forEach(function (activity_zone) {
         if (activity_zone.name === activityZone.name) {
             activityZone.id = activity_zone.id;
-            console.log('Zone Id fetched ' + activityZone.id);
+            console.log('Activity Zone Id loaded');
         }
     });
 
-    console.log('Subscribing to last_event in camera');
+    console.log('Subscribing to motion events in ' + activity_zone.name);
+
     camera.property('last_event').subscribe(function (update) {
         //Filter events with activity zones only
         if (update[0].value.activity_zone_ids) {
             //Filter events on the zone we want to monitor
             if (update[0].value.activity_zone_ids[0] == activityZone.id) {
-                console.log('Event Detected in ' + activityZone.name);
+                console.log('Event detected in ' + activityZone.name);
+                  
+                startAlarm();
 
-                raspberryPi.property().update({     // Update device status in the cloud
-                    alarm: 'on'
-                });
-
-                //start blinking the LED
-                clearInterval(blinkInterval);
-                blinkInterval = setInterval(function () {
-                    led.writeSync(led.readSync() === 0 ? 1 : 0)
-                }, 500);
             }
         }
     });
@@ -101,7 +140,7 @@ user.thng(cameraThngId).read().then(function (camera) {
 
 
 /**
- * EXIT
+ * EXIT and release resources
  */
 function exit() {
     led.unexport();
